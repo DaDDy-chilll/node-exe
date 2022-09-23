ba6b1280f3530f741d6a2cc5d86a3b249c497a01
Who <blacklistuser420@gmail.com>
2022-09-23 11:27:24 +0000

    create http server

---
+++ http-server/index.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const PORT =3000;
+const server = http.createServer();
+const friends = [
+    {
+        id:0,
+        name:'DaDDy Chill',
+    },
+    {
+        id:1,
+        name:'Tesla',
+    },{
+        id:2,
+        name:'Google',
+    },{
+        id:3,
+        name:'Microsoft',
+    },{
+        id:4,
+        name:'Cisco',
+    }
+]
+server.on('request',(req,res)=>{
+    // res.writeHead(200,{
+    //     'Content-type':'application/json'
+    // })
+    const items = req.url.split('/');
+    if(items[1] === 'friends'){
+        res.statusCode=200;
+        res.setHeader('Content-type','application/json');
+        if(items.length === 3 ){
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        }else{
+            res.end(JSON.stringify(friends))
+        }
+    }else if(items[1] === 'message'){
+        res.statusCode=200;
+        res.setHeader('Content-type','text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello World</li>');
+        res.write('<li>Welcome to my New World!</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }else if(items[1] === ''){
+        res.statusCode=200;
+        res.setHeader('Content-type','text/plain');
+        res.end('This is Home Page!');
+    }else{
+        res.statusCode=404;
+        res.setHeader('content-type','text/html');
+        res.write('<h1>Your request is not defined</h1>');
+        res.end('Please request friends and message');
+    }
+   
+})
+
+server.listen(PORT,()=>console.log(`Server is running on PORT:${PORT}`))
