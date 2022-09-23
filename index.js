142fd39ea1320c1fee793a05701a11c79486a40f
Who <blacklistuser420@gmail.com>
2022-09-23 15:44:59 +0000

    http server is finish

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ server.on('request',(req,res)=>{
     //     'Content-type':'application/json'
     // })
     const items = req.url.split('/');
-    if(items[1] === 'friends'){
+    if(req.method === 'POST' && items[1] === 'friends'){
+        req.on('data',(data)=>{
+            const firend = data.toString();
+            console.log('Request',firend);
+            friends.push(JSON.parse(firend));
+        });
+        req.pipe(res);
+    }else if(req.method === 'GET' && items[1] === 'friends'){
         res.statusCode=200;
         res.setHeader('Content-type','application/json');
         if(items.length === 3 ){
@@ -34,7 +41,7 @@ server.on('request',(req,res)=>{
         }else{
             res.end(JSON.stringify(friends))
         }
-    }else if(items[1] === 'message'){
+    }else if(req.method === 'GET' && items[1] === 'message'){
         res.statusCode=200;
         res.setHeader('Content-type','text/html');
         res.write('<html>');
@@ -46,7 +53,7 @@ server.on('request',(req,res)=>{
         res.write('</body>');
         res.write('</html>');
         res.end();
-    }else if(items[1] === ''){
+    }else if(req.method === 'GET' && items[1] === ''){
         res.statusCode=200;
         res.setHeader('Content-type','text/plain');
         res.end('This is Home Page!');
