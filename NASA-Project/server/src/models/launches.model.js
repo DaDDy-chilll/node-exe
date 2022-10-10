b753f38979efff808459a697c82e6be549daef05
Who <blacklistuser420@gmail.com>
2022-10-10 10:45:18 +0000

    get all launches finsih

--- NASA-Project/server/src/models/launches.model.js
+++ NASA-Project/server/src/models/launches.model.js
@@ -1,20 +1,24 @@
-const launches = new Map();
+const launchesData = require('./launches.mongo');
+const planets = require('./planets.mongo');
+// const launches = new Map();
 let latestFlightNumber = 100;
 const launch = {
     flightNumber:100,
     mission:'Exoplanets IS1',
     rocket:'USA1',
-    target:'Kepler-66 f',
+    target:'Kepler-442 b',
     launchDate:new Date('January 7,2030'),
     customer:['DaDDy','NASA'],
     upcoming:true,
     success:true,
 };
-launches.set(launch.flightNumber,launch);
+saveLaunch(launch);
 
-function getAllLaunches(){
-    return Array.from(launches);
+async function getAllLaunches(){
+    return await launchesData.find({},{'_id':0,'__v':0});
 }
+
+
 
 function addNewLaunches(launch){
     latestFlightNumber++;
@@ -24,6 +28,16 @@ function addNewLaunches(launch){
         customer:['DaDDy-chill','TASLA'],
         flightNumber:latestFlightNumber,
     }));
+}
+
+async function saveLaunch(launch){
+    const planet = await planets.findOne({
+        keplerName:launch.target,
+    });
+    if(!planet){
+        throw new Error('No matching planet found!')
+    }
+    await launchesData.findOneAndUpdate({flightNumber:launch.flightNumber},launch,{upsert:true});
 }
 
 module.exports = {
--- NASA-Project/server/src/routes/launches/launches.controller.js
+++ NASA-Project/server/src/routes/launches/launches.controller.js
@@ -1,7 +1,7 @@
 const {getAllLaunches,addNewLaunches} = require('../../models/launches.model');
 
-function httpGetAllLaunches(req,res){
-    res.status(200).json(getAllLaunches());
+async function httpGetAllLaunches(req,res){
+    res.status(200).json(await getAllLaunches());
 }
 
 function httpAddNewLaunches(req,res){
